8603f1ac4e308a1028393663e259160ef374f51a
Greg Roach <greg@subaqua.co.uk>
2021-06-22 15:17:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# EXAMPLE REPORT MODULE
+
+This example shows how to create a new XML report
+
+It shows how to:
+
+* use the `/resources` folder.
+* add a new entry to the reports menu.
---
+++ module.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Example report.
+ */
+
+declare(strict_types=1);
+
+namespace ExampleNamespace;
+
+require __DIR__ . '/ExampleModuleReport.php';
+
+return new ExampleModuleReport();
